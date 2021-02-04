22ccb6e13a5049069096fb0147f7c1914b33100d
Stas Lifanov <holidayonmail@gmail.com>
2021-02-04 19:26:51 +0300

    Created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import marked from "marked";
+import ReactMarkdown from "react-markdown";
+
 export default function App() {
   const [markdown, setMarkdown] = useState("# sup");
 
-  // function handleChange(e)
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
 
   return (
     <div className="app">
-      <textarea
-        onChange={(e) => setMarkdown(e.target.value)}
-        value={markdown}
-      />
+      <textarea onChange={handleChange} value={markdown} />
 
-      <div className="preview">dsadfadfadf</div>
+      {/*<div*/}
+      {/*  className="preview"*/}
+      {/*  dangerouslySetInnerHTML={{ __html: marked(markdown) }}*/}
+      {/*/>*/}
+      <ReactMarkdown className={"preview"} source={markdown} />
     </div>
   );
 }
