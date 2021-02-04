f44b4e992428da1c861b621320d1dd6f3ca60d98
Stas Lifanov <holidayonmail@gmail.com>
2021-02-04 19:04:56 +0300

    Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+
+  // function handleChange(e)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea
+        onChange={(e) => setMarkdown(e.target.value)}
+        value={markdown}
+      />
+
+      <div className="preview">dsadfadfadf</div>
     </div>
   );
 }
-
-export default App;
